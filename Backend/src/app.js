d0ed802e07b876719644778511ef102c993a3367
Vishal D Dhamu <vishaldhamu16@gmail.com>
2026-05-21 12:44:33 +0530

    cors updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,11 +6,15 @@ const interviewRouter = require('./routes/interview.routes')
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials: true
-})
-)
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://gen-ai-based-interview-prep-nine.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 
 const authRouter = require("./routes/auth.routes");
 /* Auth routes are all here */
