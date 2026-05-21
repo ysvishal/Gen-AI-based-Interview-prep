d3a14157b1ebca3a49159d591b739e65377bfad8
Vishal D Dhamu <vishaldhamu16@gmail.com>
2026-05-21 13:38:43 +0530

    cookies rules updated

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -50,7 +50,11 @@ async function registerUserController(req, res) {
     { expiresIn: "1d" },
   );
 
-  res.cookie("token", token);
+res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+});
 
   return res.status(201).json({
     message: "User registered successfully",
@@ -102,8 +106,11 @@ async function loginUserController(req, res) {
     { expiresIn: "1d" },
   );
   
-  res.cookie("token", token);
-  return res.status(200).json({
+res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+});  return res.status(200).json({
     message: "user logged in successfully",
     user: user,
     token,
